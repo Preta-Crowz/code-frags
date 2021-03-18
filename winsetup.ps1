51347bd432ca7d65711a41dabb5ed03c3e6d4523
Preta-Crowz <preta@siro.dev>
2021-03-18 12:07:38 +0900

    dd winsetup, you can run it on windows for setup your pc but it will install lot of programs.

---
+++ winsetup.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+iwr -useb get.scoop.sh | iex
+scoop bucket add extras
+scoop install anydesk authy babelmap bandizip crystaldiskinfo crystaldiskmark discord everything filezilla firefox github hosts-file-editor hwmonitor kakaotalk libreoffice-fresh locale-emulator mongodb-compass-community mp3tag musescore obs-studio plex-player poedit putty sqlitebrowser steam sublime-text sublime-merge vscode ffmpeg git deno googlechrome gcc nodejs pypy3 python vim win-acme youtube-dl
+pip install mcstatus
